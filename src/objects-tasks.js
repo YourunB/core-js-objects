1e764f0e1e60548416b1b34acd30a1015168558e
Yury Butskevich <bxyura@gmail.com>
2024-02-22 00:21:10 +0300

    feat: complete removeProperties func

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -65,8 +65,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const obj2 = { ...obj };
+  keys.forEach((el) => {
+    delete obj2[el];
+  });
+  return obj2;
 }
 
 /**
