57b9088290ac3406a89a16fd67d5d031f00c6fd4
Yury Butskevich <bxyura@gmail.com>
2024-02-23 17:47:48 +0300

    feat: complete sellTickets func

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -167,8 +167,19 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let have = 0;
+  let res = true;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] !== 25) {
+      have -= queue[i];
+      if (have < 0) {
+        res = false;
+        break;
+      }
+    } else have += queue[i];
+  }
+  return res;
 }
 
 /**
