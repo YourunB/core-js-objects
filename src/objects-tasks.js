e998c2a7138b2a2f26f599f6816359868ec33fb5
Yury Butskevich <bxyura@gmail.com>
2024-02-23 17:55:25 +0300

    fix: change unnamed func by arrow func

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -198,9 +198,7 @@ function sellTickets(queue) {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
-    return this.width * this.height;
-  };
+  this.getArea = () => this.width * this.height;
 }
 
 /**
