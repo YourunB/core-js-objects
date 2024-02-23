6a1877b9b61617fc49b98598dce231425fdfeac2
Yury Butskevich <bxyura@gmail.com>
2024-02-23 18:05:18 +0300

    feat: complete fromJSON func

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -226,8 +226,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const objSet = JSON.parse(json);
+  const objProto = Object.create(proto);
+  return Object.assign(objProto, objSet);
 }
 
 /**
